61f22cdfcd2d1835e64bee12a68da542a17beff6
Sabbir Hossain Shihab <sabbirhossainrinku@gmail.com>
2021-08-18 01:01:19 +0600

    First Commit

---
+++ component.js
@@ -0,0 +1,14 @@
+var a = 56;
+var b = 12;
+export var str = "Sabbir Hossain Shihab";
+
+var obj = {
+    name: "Jalak",
+    age: 25
+}
+
+var arr = [1,2,3,4];
+
+var hello = () => {
+    console.log("Hello Container");
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+import { str as strrr } from './component.js';
+
+var mat = require('date-format');
+
+var time = mat('hh:mm:ss.SSS', new Date());
+
+ console.log(time);
+
+console.log(strrr);
